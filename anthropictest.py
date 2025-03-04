8c070ad0b080e7f708952e278027a457fc7b4cfe
jacobchuihyc <jacobchui@uchicago.edu>
2025-03-04 14:27:55 -0600

    accuracy tests + raw/human csv files

---
+++ anthropictest.py
@@ -0,0 +1,20 @@
+import anthropic
+
+client = anthropic.Anthropic() #This initializes the client
+message = client.messages.create(
+    model="claude-3-5-sonnet-20241022", #Model specificaion (May need ot change if using pdf input)
+    max_tokens=1000, #Can use to truncate response
+    system="You are a world-class poet. Respond only with short poems.", #System prompt
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Why is the ocean salty?"
+                }
+            ]
+        }
+    ]
+)
+print(message.content) #Output form model
